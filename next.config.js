28867b22c3765422ec8f1748b5404118ac0c3a1d
Ben <benjamin.h.67110@gmail.com>
2026-01-07 18:17:27 +0100

    fix(export): trailingSlash for Apache routes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
